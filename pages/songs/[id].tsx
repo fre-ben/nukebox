6ef1c125ec7976daa431b60573819f4d3efd8ec6
fre-ben <fschocke@gmail.com>
2021-02-18 17:20:18 +0100

    Add getSong function + wip elements

--- pages/songs/[id].tsx
+++ pages/songs/[id].tsx
@@ -1,15 +1,45 @@
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { APISong, getSong } from "../../utils/api";
+import styles from "../../styles/SongPage.module.css";
 
 export default function Song() {
   const router = useRouter();
   const { id } = router.query;
 
+  const [song, setSong] = useState<APISong>(null);
+
+  useEffect(() => {
+    console.log(id);
+    if (typeof id !== "string") {
+      return;
+    }
+    getSong(id).then((newSong) => {
+      setSong(newSong);
+    });
+  }, [id]);
+
+  if (!song) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Link href="/">
-      <a>
-        <div>Song: {id}</div>
-      </a>
-    </Link>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Link href="/">
+          <a>
+            <div> &lt;</div>
+          </a>
+        </Link>
+        <span>Now Playing</span>
+      </div>
+
+      <div>
+        <img src={song.imgSrc} className={styles.image}></img>
+        <p>Title: {song.title}</p>
+        <p>Artist: {song.artist}</p>
+      </div>
+    </div>
   );
 }
