129d6f1f68f7d3e58cd384305c37683ed44b4157
fre-ben <fschocke@gmail.com>
2021-02-19 10:51:03 +0100

    Add onClick attribute with history.back() method

--- components/SongPageHeader.tsx
+++ components/SongPageHeader.tsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
 import styles from "../styles/SongPage.module.css";
 
 export default function SongPageHeader() {
+  const back = () => {
+    history.back();
+  };
+
   return (
     <div className={styles.header}>
-      <img src="/arrow-left.svg" />
+      <img src="/arrow-left.svg" onClick={back} />
 
       <span>Now Playing</span>
     </div>
