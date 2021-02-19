906964829271552d4cb00dc9c6d1419ad6f99c16
fre-ben <fschocke@gmail.com>
2021-02-19 10:38:46 +0100

    🔥 Add history.back() to arrow in header, remove old link

--- components/SongPageHeader.tsx
+++ components/SongPageHeader.tsx
@@ -4,11 +4,8 @@ import styles from "../styles/SongPage.module.css";
 export default function SongPageHeader() {
   return (
     <div className={styles.header}>
-      <Link href="/">
-        <a>
-          <img src="/arrow-left.svg" />
-        </a>
-      </Link>
+      <img src="/arrow-left.svg" />
+
       <span>Now Playing</span>
     </div>
   );
