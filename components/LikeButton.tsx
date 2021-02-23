4621e7144355b9b08fb5ff010f9d32e46ef5e3d5
fre-ben <fschocke@gmail.com>
2021-02-23 17:11:55 +0100

    :sparkles: Add localstorage function to like button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useRouter } from "next/dist/client/router";
+import { useEffect, useState } from "react";
 import styles from "../styles/Toolbar.module.css";
 
 export default function LikeButton() {
-  const [heart, setHeart] = useState(false);
+  const [heart, setHeart] = useState(null);
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (typeof id !== "string" || heart === null) {
+      return;
+    }
+    if (heart) {
+      localStorage.setItem("likedSong", id);
+    } else {
+      localStorage.removeItem("likedSong");
+    }
+  }, [heart, id]);
+
+  useEffect(() => {
+    if (typeof id !== "string") {
+      return;
+    }
+    setHeart(id === localStorage.getItem("likedSong"));
+  }, [id]);
 
   const handleLikeButtonClick = () => {
     setHeart(!heart);
   };
+
   return (
     <img
       onClick={handleLikeButtonClick}
