a377914ff848b8d5d10526783b97df0dcc27a578
fre-ben <fschocke@gmail.com>
2021-02-23 18:59:02 +0100

    :sparkles: Add functionality to visitcounter

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styles from "../styles/greeting.module.css";
 
 type Props = {
@@ -5,6 +6,22 @@ type Props = {
 };
 
 export default function Greeting(props: Props) {
+  function VisitsCount() {
+    if (typeof localStorage === "undefined") {
+      return <div>localStorage is not supported by Server-Side-Rendering</div>;
+    }
+    const viewCounter = localStorage.getItem("visits");
+    return viewCounter;
+  }
+  let counter = Number(VisitsCount());
+
+  useEffect(() => {
+    counter++;
+    const counterString = counter.toString();
+    localStorage.setItem("visits", counterString);
+    console.log(counter);
+  }, []);
+
   return (
     <>
       <p className={styles.hello}>
@@ -12,7 +29,7 @@ export default function Greeting(props: Props) {
       </p>
       <p className={styles.counter}>
         You&apos;ve been here
-        <span className={styles.counterNumber}> 11</span> times!
+        <span className={styles.counterNumber}> {counter}</span> times!
       </p>
     </>
   );
