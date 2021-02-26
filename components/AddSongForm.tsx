711c1ca1b72da2c0d7b54077895d2c7368be1a9f
fre-ben <fschocke@gmail.com>
2021-02-26 10:56:06 +0100

    Add form component and include in add page

---
+++ components/AddSongForm.tsx
@@ -0,0 +1,5 @@
+import styles from "../styles/AddSongForm.module.css";
+
+export default function AddSongForm() {
+  return <form className={styles.formContainer}>Test</form>;
+}
--- pages/add.tsx
+++ pages/add.tsx
@@ -1,9 +1,11 @@
+import AddSongForm from "../components/AddSongForm";
 import styles from "../styles/Add.module.css";
 
 export default function Add() {
   return (
     <div className={styles.container}>
       <h2>Add a new song:</h2>
+      <AddSongForm />
     </div>
   );
 }
