65456c7570dd3a628218cba435607c10243e5c78
fre-ben <fschocke@gmail.com>
2021-02-26 14:46:24 +0100

    :sparkles: Add back button to Add Song page

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import AddPageHeader from "../components/AddPageHeader";
 import AddSongForm from "../components/AddSongForm";
 import styles from "../styles/Add.module.css";
 
 export default function Add() {
   return (
     <div className={styles.container}>
+      <AddPageHeader />
       <h2>Add a new song:</h2>
       <AddSongForm />
     </div>
