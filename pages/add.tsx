7fefd24ae9078389ff5af1dfec1eec998a23dafc
fre-ben <fschocke@gmail.com>
2021-02-26 10:51:04 +0100

    :sparkles: Add CSS file and page headline

--- pages/add.tsx
+++ pages/add.tsx
@@ -2,8 +2,8 @@ import styles from "../styles/Add.module.css";
 
 export default function Add() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div className={styles.container}>
+      <h2>Add a new song:</h2>
+    </div>
   );
 }
